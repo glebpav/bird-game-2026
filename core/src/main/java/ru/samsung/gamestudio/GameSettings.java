1c7100f91eac7836e123e5403fe9cab875133bbb
gleb <glebpavlyuk@gmail.com>
2026-02-04 17:36:27 +0300

    edit bird speed

--- core/src/main/java/ru/samsung/gamestudio/GameSettings.java
+++ core/src/main/java/ru/samsung/gamestudio/GameSettings.java
@@ -2,7 +2,7 @@ package ru.samsung.gamestudio;
 
 public class GameSettings {
 
-    public static int BIRD_SPEED = 4;
+    public static int BIRD_SPEED = 5;
     public static int BIRD_JUMP_HEIGHT = 150;
 
 }
